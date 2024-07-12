6a1a52e4c6c82c67efd5196ed25807bc2f85761c
Muinjon2003 <kholovmuin5@gmail.com>
2024-07-12 14:58:46 +0300

    fixed

--- lib/pages/my_splash_page.dart
+++ lib/pages/my_splash_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterinstagram/components/navigation_manu.dart';
+import 'package:flutterinstagram/pages/login_page.dart';
 
 class MySplashPage extends StatefulWidget {
   const MySplashPage({super.key});
@@ -16,7 +16,7 @@ class _MySplashPageState extends State<MySplashPage> {
         child: GestureDetector(
             onTap: () {
               Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => NavigationMenu()));
+                  MaterialPageRoute(builder: (context) => LoginPage()));
             },
             child: Text("Splash screen page")),
       ),
